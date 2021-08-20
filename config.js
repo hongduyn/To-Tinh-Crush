0205373ea0fec1972da189640a8af60bb31c209d
hongduyn <89238156+hongduyn@users.noreply.github.com>
2021-08-20 11:56:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Hí các chị đại!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnIntro: 'Híhí',
+    title: 'Phải chăng tui đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng tui đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích  <33',
+    btnNo: 'Không, chị gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu tui <3',
+    btnReply: 'Gửi cho tui <3',
+    reply: 'tại vì Duyn xinh gái đáng iu,dễ thương cutephomaique nhất trên đời',
+    mess: 'Tui biết mà 🥰. iu các chị đại nhiều nhiều 😘😘',
+    messDesc: 'iu bé duyn quá trời.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/duyen.duongthihong/
 }
