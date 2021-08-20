5cef844958a292a2e65cfb7642d0dcc3e5527147
hongduyn <89238156+hongduyn@users.noreply.github.com>
2021-08-20 21:40:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là iu chết đi đươc  <33',
+    btnYes: ' iu anh chết đi đươc  <33',
     btnNo: 'Không, nha:3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
